a9dc018bc56c1b899da93e269706679a53c88459
gknave <gknave@vt.edu>
2016-12-22 13:52:05 -0700

    Update setup.py
    Updated description and url in setup.py

--- setup.py
+++ setup.py
@@ -1,8 +1,8 @@
 from setuptools import setup
 setup(name='manifoldid',
     version='0.1',
-	description='Package for finding attracting manifolds in phase space',
-	url='',
+	description='Scratch package for methods to identify manifolds, particularly within simple glider models.',
+	url='https://github.com/gknave/ManifoldID',
 	author='Gary Nave'
 	author_email='gknave@vt.edu',
 	packages=['manifoldid'],
